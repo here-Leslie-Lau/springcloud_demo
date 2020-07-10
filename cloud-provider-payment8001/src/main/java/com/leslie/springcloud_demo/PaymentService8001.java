a116939d97b92fe94acbccb2213793d919fec459
here-Leslie-Lau <804738464@qq.com>
2020-07-10 20:49:59 +0800

    入驻7001Eureka服务注册中心

--- cloud-provider-payment8001/src/main/java/com/leslie/springcloud_demo/PaymentService8001.java
+++ cloud-provider-payment8001/src/main/java/com/leslie/springcloud_demo/PaymentService8001.java
@@ -3,8 +3,10 @@ package com.leslie.springcloud_demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @MapperScan(basePackages = "com.leslie.springcloud_demo.mapper")
 public class PaymentService8001 {
     public static void main(String[] args) {
