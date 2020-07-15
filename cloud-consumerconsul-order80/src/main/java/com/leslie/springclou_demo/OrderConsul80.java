35adf0cac6f831addbd7782c119484ff77d7d11b
here-Leslie-Lau <804738464@qq.com>
2020-07-15 13:31:50 +0800

    新增模块consumerconsul80,并加入consul服务注册中心

---
+++ cloud-consumerconsul-order80/src/main/java/com/leslie/springclou_demo/OrderConsul80.java
@@ -0,0 +1,14 @@
+package com.leslie.springclou_demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class OrderConsul80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsul80.class,args);
+    }
+
+}
