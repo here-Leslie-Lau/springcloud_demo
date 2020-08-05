96d6b6d3f85eb3d5ce332c4a1feca5e76dd2ba57
here-Leslie-Lau <804738464@qq.com>
2020-08-05 16:40:24 +0800

    手动版动态更新配置

--- cloud-config-client-3355/src/main/java/com/leslie/springcloud_demo/ConfigClientMain3355.java
+++ cloud-config-client-3355/src/main/java/com/leslie/springcloud_demo/ConfigClientMain3355.java
@@ -2,10 +2,12 @@ package com.leslie.springcloud_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class ConfigClientMain3355 {
 
     public static void main(String[] args) {
