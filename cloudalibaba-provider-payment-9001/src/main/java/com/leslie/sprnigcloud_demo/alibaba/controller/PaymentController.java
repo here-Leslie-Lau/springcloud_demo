1cea1c7fb2f1f8d235a52535d9fa75e76d2d4580
here-Leslie-Lau <804738464@qq.com>
2020-08-25 09:02:28 +0800

    init commit - nacos微服务提供端9001

---
+++ cloudalibaba-provider-payment-9001/src/main/java/com/leslie/sprnigcloud_demo/alibaba/controller/PaymentController.java
@@ -0,0 +1,20 @@
+package com.leslie.sprnigcloud_demo.alibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/nacos/payment/{id}")
+    public String getPayment(@PathVariable Integer id){
+        return "nacos register, port=" + port + "\t id:" + id;
+    }
+}
